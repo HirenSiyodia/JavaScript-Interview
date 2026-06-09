6a8c396d600a12bc9990a3d0cc46cc8eca8cfbdf
Hiren Siyodia | Front End Developer <siyodiahiren7@gmail.com>
2026-06-09 16:18:46 +0000

    Add method()

---
+++ Arrays/Add/index.js
@@ -0,0 +1,16 @@
+//push();
+const numbers = [1, 2, 3,]
+const resNum = numbers.push(4);
+console.log(resNum); // 4
+console.log(numbers); // [ 1, 2, 3, 4 ]
+
+// concat():
+const arr1 = [1, 2]
+const arr2 = [3, 4]
+const resConcat = arr1.concat(arr2);
+console.log(resConcat); // [ 1, 2, 3, 4 ]
+
+const arr3 = [4, 5]
+const res = arr3.concat(8);
+console.log(res);
+// [ 4, 5, 8 ]
